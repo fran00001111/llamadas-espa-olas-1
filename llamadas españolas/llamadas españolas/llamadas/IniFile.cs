5971b734edd03abbc14b3da83a79f0b73ec2fa9d
fran00001111 <110295197+fran00001111@users.noreply.github.com>
2023-04-15 22:56:09 -0300

    Add files via upload

---
+++ llamadas españolas/llamadas españolas/llamadas/IniFile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rage;
+using LSPD_First_Response.Mod.API;
+namespace llamagas_españa.llamadas
+{
+   public class Main
+    {
+    }
+}
